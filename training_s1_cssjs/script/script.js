886926eb77b75a160c8196433ede90737dd12a1c
angegoua <47737643+angegoua@users.noreply.github.com>
2019-07-04 23:21:15 +0200

    Add files via upload

---
+++ training_s1_cssjs/script/script.js
@@ -0,0 +1,32 @@
+const cursor = document.querySelector('.cursor');
+const hoverLinks = document.querySelectorAll('a');
+    
+    
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: "+(e.pageY -10)+"px; left: "+(e.pageX -10)+"px;")
+    })
+    
+    for(let i=0; i<hoverLinks; i++){
+        hoverLinks[i].addEventListener('mouseover', function() {
+    cursor.style.width=0+'px'
+    })
+    }
+
+    var t1 = new TimelineMax({paused : true});
+
+            t1.to(".block", 1.2, {
+                  width: "25%",
+                  ease: Power4.easeInOut
+            });
+
+            t1.staggerFrom(".menu ul li", 0.8, {y: 40, opacity: 0, ease: Power2.easeOut}, 0.2);
+
+            t1.reverse();
+            $(document).on("click", ".menu-btn", function() {
+                  t1.reversed(!t1.reversed());
+                  
+            });
+
+            
+    
